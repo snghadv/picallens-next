6e79b33c8ce7eadf90c68d1d7fba56e3ac533fd4
snghadv <singh8859saurabh@gmail.com>
2022-11-10 20:50:54 +0530

    ...

--- pages/_app.js
+++ pages/_app.js
@@ -2,9 +2,16 @@ import Navbar from '../components/Navbar';
 import '../styles/globals.css';
 import 'font-awesome/css/font-awesome.min.css';
 import { Analytics } from '@vercel/analytics/react';
+import Head from 'next/head';
 
 function MyApp({ Component, pageProps }) {
   return <>
+    <Head>
+      <link rel="shortcut icon" href="/logo192.svg" />
+      <link rel="apple-touch-icon" sizes="180x180" href="/logo192.svg" />
+      <link rel="icon" type="image/png" sizes="32x32" href="/logo192.svg" />
+      <link rel="icon" type="image/png" sizes="16x16" href="/logo192.svg" />
+    </Head>
     <Navbar />
     <Component {...pageProps} />
     <Analytics />
