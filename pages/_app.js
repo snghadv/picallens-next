27a0ba539c33319fc7d79cb8041430b691652f09
snghadv <singh8859saurabh@gmail.com>
2022-11-10 20:37:29 +0530

    lfjlkjlkf

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,13 @@
 import Navbar from '../components/Navbar';
 import '../styles/globals.css';
 import 'font-awesome/css/font-awesome.min.css';
+import { Analytics } from '@vercel/analytics/react';
 
 function MyApp({ Component, pageProps }) {
   return <>
     <Navbar />
     <Component {...pageProps} />
+    <Analytics />
   </>
 }
 
