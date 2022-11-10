d98a30db8a6ed835daab9e2afc5b5d12f3847cbc
snghadv <singh8859saurabh@gmail.com>
2022-11-10 20:55:32 +0530

    .....

--- pages/_app.js
+++ pages/_app.js
@@ -3,6 +3,7 @@ import '../styles/globals.css';
 import 'font-awesome/css/font-awesome.min.css';
 import { Analytics } from '@vercel/analytics/react';
 import Head from 'next/head';
+import Script from 'next/script';
 
 function MyApp({ Component, pageProps }) {
   return <>
@@ -11,6 +12,17 @@ function MyApp({ Component, pageProps }) {
       <link rel="apple-touch-icon" sizes="180x180" href="/logo192.svg" />
       <link rel="icon" type="image/png" sizes="32x32" href="/logo192.svg" />
       <link rel="icon" type="image/png" sizes="16x16" href="/logo192.svg" />
+      <Script async src="https://www.googletagmanager.com/gtag/js?id=G-BV97DX5Z73" strategy='afterInteractive'/>
+      <Script>
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag() {
+            dataLayer.push(arguments);
+          }
+          gtag('js', new Date());
+          gtag('config', 'G-BV97DX5Z73');
+        `}
+      </Script>
     </Head>
     <Navbar />
     <Component {...pageProps} />
